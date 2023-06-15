157270c6a8e104c58752fb64804ea9452a0c7b4b
Bopha3949 <phila402@gmail.com>
2023-06-15 09:55:49 -0700

    lol

---
+++ alx-low_level_programming/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+includ <stdion.h>
+/**
+ * main - Entry point
+ *
+ * return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
